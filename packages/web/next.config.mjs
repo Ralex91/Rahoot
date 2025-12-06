cf4ef8674d1c5a2423331a51ef12a9f61879df4b
Ralex <95540504+Ralex91@users.noreply.github.com>
2025-12-06 14:04:25 +0100

    chore(package): update

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -2,9 +2,6 @@ const nextConfig = {
   output: "standalone",
   productionBrowserSourceMaps: false,
   transpilePackages: ["packages/*", "@t3-oss/env-nextjs"],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 export default nextConfig
